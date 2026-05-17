b2724c0348bdf36dd03abe4661a8fb4c7a2371cc
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-05-17 02:27:38 +0200

    [Pipeline] GeneratedFile4.kt

--- legacy_code.txt/GeneratedFile4.kt
+++ legacy_code.txt/GeneratedFile4.kt
@@ -1,5 +1,3 @@
-This is a legacy file.
-It contains some old notes.
-Line 3.
-Line 4.
-We need to replace all of this with a proper README.
+# Test Project
+Этот репозиторий используется для краш-теста AI пайплайна.
+Старый код удален.
