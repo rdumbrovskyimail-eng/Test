42fb8394441edb161948db8cdf0a23c8103b11da
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-05-17 03:03:30 +0200

    [Pipeline] LegacyUtils.kt

--- app/src/main/java/com/test/app/utils/LegacyUtils.kt
+++ app/src/main/java/com/test/app/utils/LegacyUtils.kt
@@ -1,14 +1,3 @@
 package com.test.app.utils
 
-class LegacyUtils {
-
-    fun doSomeHeavyWork() {
-        Thread.sleep(1000)
-        println("Heavy work done on main thread! This is very bad!")
-    }
-
-    fun parseJsonManually(json: String): Map<String, String> {
-        // very old implementation
-        return emptyMap()
-    }
-}
+// Deprecated. Use modern extensions instead.
