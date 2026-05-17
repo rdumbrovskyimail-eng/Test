e2e2f8c8df376157893817d571056e89492cd4fd
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-05-17 02:27:30 +0200

    [Pipeline] GeneratedFile3.kt

--- build.gradle.kts/GeneratedFile3.kt
+++ build.gradle.kts/GeneratedFile3.kt
@@ -11,11 +11,12 @@ android {
         applicationId = "com.test.app"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
 }
