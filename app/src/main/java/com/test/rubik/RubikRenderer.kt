470773b57fa17f45f0e17631794d298bc25a0840
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-05-17 03:43:09 +0200

    [Pipeline] RubikRenderer.kt

--- app/src/main/java/com/test/rubik/RubikRenderer.kt
+++ app/src/main/java/com/test/rubik/RubikRenderer.kt
@@ -6,6 +6,7 @@ import android.opengl.Matrix
 import android.os.SystemClock
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
+import kotlin.random.Random
 
 class RubikRenderer : GLSurfaceView.Renderer {
 
@@ -21,32 +22,39 @@ class RubikRenderer : GLSurfaceView.Renderer {
         GLES20.glClearColor(0.1f, 0.1f, 0.1f, 1.0f)
         GLES20.glEnable(GLES20.GL_DEPTH_TEST)
         
-        // Инициализируем всю модель кубика Рубика
         rubikModel = RubikModel()
     }
 
     override fun onDrawFrame(unused: GL10) {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT or GLES20.GL_DEPTH_BUFFER_BIT)
 
-        // Отдаляем камеру, чтобы весь куб 3x3x3 поместился в кадр
+        // Шаг анимации вращения слоев
+        rubikModel.animate()
+
+        // Если кубик сейчас не анимирует слой, запускаем случайный поворот!
+        if (!rubikModel.isAnimating) {
+            val randomAxis = Axis.values()[Random.nextInt(3)]
+            val randomSlice = Random.nextInt(3) - 1 // -1, 0 или 1
+            val randomDir = if (Random.nextBoolean()) 1 else -1
+            rubikModel.startRotation(randomAxis, randomSlice, randomDir)
+        }
+
         Matrix.setLookAtM(viewMatrix, 0, 0f, 4f, -12f, 0f, 0f, 0f, 0f, 1f, 0f)
         Matrix.multiplyMM(vPMatrix, 0, projectionMatrix, 0, viewMatrix, 0)
 
-        // Вращаем весь кубик Рубика целиком по диагональной оси для демонстрации
-        val time = SystemClock.uptimeMillis() % 10000L
-        val angle = 0.036f * time.toInt()
+        // Медленно вращаем весь кубик в пространстве, чтобы видеть его со всех сторон
+        val time = SystemClock.uptimeMillis() % 12000L
+        val angle = 0.030f * time.toInt()
         
         Matrix.setRotateM(rotationMatrix, 0, angle, 1f, 1f, 1f)
         Matrix.multiplyMM(scratch, 0, vPMatrix, 0, rotationMatrix, 0)
 
-        // Отрисовываем модель
         rubikModel.draw(scratch)
     }
 
     override fun onSurfaceChanged(unused: GL10, width: Int, height: Int) {
         GLES20.glViewport(0, 0, width, height)
         val ratio: Float = width.toFloat() / height.toFloat()
-        // Настройка перспективы
         Matrix.frustumM(projectionMatrix, 0, -ratio, ratio, -1f, 1f, 3f, 20f)
     }
 }
