36e5a56a1efc85335b1ee518c3df0cad9f1b2969
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-05-17 02:27:29 +0200

    [Pipeline] MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,18 +1,13 @@
 package com.test.app
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // TODO: Initialize UI
-        println("App started")
-    }
-
-    fun doSomethingOld() {
-        val x = 10
-        val y = 20
-        println(x + y)
+        Log.d("MainActivity", "App started safely")
     }
 }
